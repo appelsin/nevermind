1cdc60e695506d7ee98106de583ad139686ad667
Roman Exempliarov <rexe@ya.ru>
2014-09-17 18:58:04 +0400

    Delete nevermind_spec.rb

--- spec/nevermind_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Nevermind do
-  xit "broccoli is gross" do
-    Foodie::Food.portray("Broccoli").should eql("Gross!")
-  end
-
-  xit "anything else is delicious" do
-    Foodie::Food.portray("Not Broccoli").should eql("Delicious!")
-  end
-end
